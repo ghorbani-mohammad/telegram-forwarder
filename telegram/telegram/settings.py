f8eb6f3704e67bb8631ec70e95f23c955b400b00
ghorbani <aali361@gmail.com>
2020-08-13 03:53:29 +0430

    add db

--- telegram/telegram/settings.py
+++ telegram/telegram/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'telegram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'telegram_db',
+        'PORT': '5432',
     }
 }
 
