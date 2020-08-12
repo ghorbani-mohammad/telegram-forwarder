609055f807499d1046852dddc64ec927b0ed1681
ghorbani <aali361@gmail.com>
2020-08-12 23:06:06 +0430

    add celery settings

--- telegram/telegram/settings.py
+++ telegram/telegram/settings.py
@@ -120,3 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# CELERY
+BROKER_URL = 'redis://telegram_redis:6379'
+CELERY_RESULT_BACKEND = 'redis://telegram_redis:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Tehran'
